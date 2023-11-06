b654c22de6f93a997f43c8fb933d4f7ea5857c01
sandhiya121298 <140143071+sandhiya121298@users.noreply.github.com>
2023-11-06 20:30:55 +0530

    Add files via upload

---
+++ check_weekdays.py
@@ -0,0 +1,49 @@
+##check weekday
+
+n=int(input("enter the value of weeksdays: "))
+if(n==1):
+    print("monday")
+elif(n==2):
+    print("tuesday")
+elif(n==3):
+    print("wednesday")
+elif(n==4):
+    print("thrusday")
+elif(n==5):
+    print("friday")
+elif(n==6):
+    print("saturday")
+elif(n==7):
+    print("sunday")
+else:
+    print("Invalid Input! Please enter week number between 1-7.")
+
+##check months    
+
+n=int(input("enter the value of weeksdays: "))
+if(n==1):
+    print("january")
+elif(n==2):
+    print("feberuary")
+elif(n==3):
+    print("march")
+elif(n==4):
+    print("april")
+elif(n==5):
+    print("may")
+elif(n==6):
+    print("june")
+elif(n==7):
+    print("july")
+elif(n==8):
+    print("august")
+elif(n==9):
+    print("september")
+elif(n==10):
+    print("october")
+elif(n==11):
+    print("november")
+elif(n==12):
+    print("december")    
+else:
+    print("Invalid Input! Please enter week number between 1-12.")
---
+++ find_max.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the value a: "))
+b=int(input("Enter the value b: "))
+c=int(input("Enter the value c: "))
+if(a>b):
+    print("a is maximum")
+elif(b>c):
+    print("b is maximum")
+else:
+    print("c is maximum")
+
